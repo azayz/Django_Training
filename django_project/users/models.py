cbe554017e5d89115751101e3ed60f3247f2862f
Aziz Belaweid <azizbellaweid@hotmail.com>
2020-11-18 16:57:31 +0100

    Added update profile picture feature

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 
 class Profile(models.Model) :
@@ -9,3 +10,13 @@ class Profile(models.Model) :
 
     def __str__(self) : 
         return f'{self.user.username} Profile'
+
+    def save(self,*args,**kwargs) :
+        super().save(*args,**kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 150 or img.width > 150 :
+            output_size = (150,150)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
