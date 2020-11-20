b481e2ee6e6cc6fee74fef1115a48275f5cdca23
Aziz Belaweid <azizbellaweid@hotmail.com>
2020-11-20 11:35:31 +0100

    Added create, update and Delete posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView,DetailView,CreateView , UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
 def home(request) :
@@ -12,4 +14,46 @@ def home(request) :
     return render(request,'blog/home.html',context=context)
 
 def about(request) :
-    return render(request,'blog/about.html',{'title' : 'About'})  
+    return render(request,'blog/about.html',{'title' : 'About'})
+
+
+class PostListView(ListView) :
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'     
+    ordering = ['-date_posted'] 
+
+class PostDetailView(DetailView) : 
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostUpdateView(LoginRequiredMixin,UpdateView,UserPassesTestMixin) : 
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form) :
+        form.instance.author = self.request.user
+        return super().form_valid(form) 
+
+    def test_func(self) :
+        post = self.get_object()
+        if self.request.user == post.author :
+            return True
+        return False
+
+class PostCreateView(LoginRequiredMixin,CreateView) : 
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form) :
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostDeleteView(DeleteView,LoginRequiredMixin,UserPassesTestMixin) :
+    model = Post
+    success_url = '/'
+    def test_func(self) :
+        post = self.get_object()
+        if self.request.user == post.author :
+            return True
+        return False 
